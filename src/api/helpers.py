f44e199d715b281841b6e449a330565742048fdb
Prashant Tiwari <prashanttiwari247@gmail.com>
2019-11-11 17:01:22 +0000

    Make add memory intensive and sleepy.

--- src/api/helpers.py
+++ src/api/helpers.py
@@ -1,4 +1,4 @@
-
+from time import sleep
 from celery import Celery
 
 from src import logger
@@ -10,5 +10,7 @@ app = Celery("tasks", broker=redis_url, backend=redis_url)
 
 @app.task
 def add(data):
+    fat_data = [1]*15000
+    sleep(60 * 5)
     logger.info("Adding {} and {}".format(data['a'], data['b']))
     return data['a'] + data['b']
